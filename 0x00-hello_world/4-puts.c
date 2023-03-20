5314ccab1f2cb9da3da7accd7bf9db55333b251c
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-03-20 10:18:05 -0700

    gccc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
-/*puts writes a string and a trail of new line in the std.io */
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Returns 0
+ */
 int main(void)
 {
+	/*put writes a stric and a trail of new line in the std.io*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
-/*printf prints output according to arguments*/
+
+/**
+ * main - the prints "with proper grammar, but the outcome is a piece of art,"
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,"\n);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (o);
 }
