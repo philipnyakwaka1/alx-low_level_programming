7c0195d687688a7666d7b731581d12131b0ca67f
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-03-20 08:17:27 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+/*puts writes a string and a trail of new line in the std.io */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+/*printf prints output according to arguments*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,"\n);
+	return (o);
+}
