a01e5d155a3eb9da4143571047abe1c370ee16a8
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-03-22 17:57:11 +0000

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/**
+ * main - prints combo of 2 digits
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x < 9; x++)
+	{
+		for (y = x + 1; y < 10; y++)
+		{
+			putchar(x + '0');
+			putchar(y + '0');
+			if (x == 8 && y == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
