1c6a3b6778c52272253dead8ea82691a9daec806
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-03-22 16:19:09 +0000

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - prints 0_9
+ *
+ * Return: returns 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
