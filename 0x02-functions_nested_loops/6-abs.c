c481751796a9245123d993642280978283c8d0ce
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-03-23 14:25:15 +0000

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - returns absolute value of an interger
+ * @i: interger
+ *
+ * Return: absolute value of i
+ */
+int _abs(int i)
+{
+	if (i > 0)
+		return (i);
+	else
+		return (-i);
+}
