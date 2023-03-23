5ef17be292976a8ca8091026a3b90f8ed37a7356
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-03-23 14:59:18 +0000

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * jack_bauer - prints mnutes of the day
+ *
+ * Return: 24hr clock
+ */
+void jack_bauer(void)
+{
+	int m, h;
+
+	h = 0;
+	while (h < 24)
+	{
+		m = 0;
+		while (m < 60)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+			m++;
+		}
+		h++;
+	}
+}
+
