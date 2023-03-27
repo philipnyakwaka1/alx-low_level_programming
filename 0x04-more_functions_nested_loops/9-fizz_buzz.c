7c97ca28990323bfc683b14d066b4b5214b7a14e
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-03-27 10:47:05 +0000

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - FizzBuzz test
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
