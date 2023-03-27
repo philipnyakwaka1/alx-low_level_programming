cc2518d7a56b4a47e06b7cd47e0cb6cd21ab3db2
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-03-27 08:09:40 +0000

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,16 +6,14 @@
  */
 void print_most_numbers(void)
 {
-	char c;
+	int a;
 
-	for (c = '0'; c <= '9'; c++)
+	for (a = 0; a <= 9; a++)
 	{
-		if (c != '2')
-		{
-			if (c != '4')
-			{
-				_putchar(c);
-			}
-		}
+		if ((a == 2) || (a == 4))
+			continue;
+		else
+			_putchar(a + '0');
+	}
 	_putchar('\n');
 }
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * more_numbers - prints 0 to 14 10x
+ *
+ * Return: returns 0 to 14
+ */
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; 
