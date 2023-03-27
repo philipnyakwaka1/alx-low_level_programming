777ed44842919a784944eb119be28d6c9b98a83b
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-03-27 09:46:19 +0000

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,12 +7,20 @@
  */
 void print_line(int n)
 {
-	int i;
 
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
 	else
+	{
+		int i;
+
 		for (i = 1; i <= n; i++)
+		{
 			_putchar('_');
+		}
 		_putchar('\n');
+	}
+
 }
