e1a7c9691c652a522afcc11f2fc0fa22cdc71602
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-03-27 10:59:10 +0000

    9-fizz_buzz.c1

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,11 +17,11 @@ int main(void)
 		}
 		else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf(" Buzz ");
+			printf(" Buzz");
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf(" FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else if (i == 1)
 		{
