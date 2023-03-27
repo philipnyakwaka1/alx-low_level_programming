9e6663cc77adf5c88abf4633544ab47a86586c81
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-03-27 10:01:09 +0000

    7-print_diagonal.c, 2

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,7 +7,6 @@
  */
 void print_diagonal(int n)
 {
-	int x, y;
 
 	if (n <= 0)
 	{
@@ -15,13 +14,17 @@ void print_diagonal(int n)
 	}
 	else
 	{
-		for (x = 1; x <= n; x++)
+		int x, y;
+
+		for (x = 1; x < n; x++)
 		{
-			for (y = 1; y <= x; y++)
+			for (y = 1; y < n; y++)
 			{
-				_putchar(' ');
+				if (y == 1)
+					_putchar('\\');
+				else if (y < x)
+					_putchar(' ');
 			}
-			_putchar('\\');
 			_putchar('\n');
 		}
 	}
