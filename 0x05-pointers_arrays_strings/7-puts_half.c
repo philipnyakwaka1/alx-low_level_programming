51fd5d621ddecb14406a6028546273827548ed2d
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-03-28 10:13:33 +0000

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,5 +5,17 @@
  */
 void puts_half(char *str)
 {
-	exit
+	int x, y, z;
 
+	z = 0;
+
+	for (x = 0; str[x] != '\0'; x++)
+		z++;
+		y = (z / 2);
+
+		if ((z % 2) == 1)
+			y = ((z + 1) / 2);
+		for (x = y; str[x] != '\0'; x++)
+			_putchar('\n');
+		_putchar('\n');
+}
