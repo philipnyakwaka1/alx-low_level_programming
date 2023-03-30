6c9e8d91b213a2a1ac5f75c5163277126c6b6d3c
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-03-30 11:46:59 +0000

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,14 +10,18 @@ char *_strcat(char *dest, char *src)
 {
 	int x, y;
 
-	for (x = 0; dest[x] != '\0'; x++)
+	x = 0;
+	while (dest[x] != '\0')
 	{
-		for (y = 0; src[y] != '\0'; y++)
-		{
-			dest[x] = src[y];
-			x++;
-		}
+		x++;
 	}
+	y = 0;
+	while (src[y] != '\0')
+	{
+		dest[x] = src[y];
+		x++;
+		y++;
+	}
+	dest[x] = '\0';
 	return (dest);
 }
-
