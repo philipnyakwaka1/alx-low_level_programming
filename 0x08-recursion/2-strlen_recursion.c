6b07d47823f622efd50cb3c602913badafac1ee4
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-06-14 13:54:51 +0000

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,6 +7,7 @@
 int _strlen_recursion(char *s)
 {
 	int len;
+
 	len = 0;
 
 	if (*s == '\0')
@@ -16,4 +17,4 @@ int _strlen_recursion(char *s)
 	len++;
 	len += _strlen_recursion(s + 1);
 	return (len);
-}	
+}
