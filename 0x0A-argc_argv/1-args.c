20d5800740f6dede0658caf4b41836b594c8b8cb
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-06-18 10:53:53 +0000

    1-args.c2

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,11 +3,12 @@
 /**
  * main - prints the number of command line arguments passed to it
  * @argc: number of command line arguments
+ * @argv: an array of arguments
  * Return: 0 is success
  */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d", argc);
+	printf("%d", argc - 1);
 	putchar('\n');
 	return (0);
 }
