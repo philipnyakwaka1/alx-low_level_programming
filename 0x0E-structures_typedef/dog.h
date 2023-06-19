ccc547ff04a4d2d8a38a5905bc0b89f6fd766884
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-06-19 21:23:00 +0000

    created a struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - holds dog details
+ * @name: dog name
+ * @age: dog's age
+ * @owner: dog owner
+ *
+ * Description: This struct will store information about a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
