372dfc24c5311df317e0325bb16502b31135881b
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-06-21 14:55:46 +0000

    created a function with a function pointer as parameter

---
+++ 0x0F-function_pointers/_putchar.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+#include<unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: character to be printed
+ * Return: 1 on success -1 on error
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
