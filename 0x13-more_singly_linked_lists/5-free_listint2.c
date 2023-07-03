e446226885eae6060759cf9d6916bd253af9c536
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-07-03 17:59:37 +0000

    freed listint_t

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,12 +7,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	temp = *head;
-	while (temp != NULL && head != NULL)
+	if (head)
 	{
-		temp = *head->next;
-		free(*head);
-		*head = temp;
+		temp = *head;
+		while (temp != NULL)
+		{
+			temp = *temp->next;
+			free(*head);
+			*head = temp;
+		}
+		*head = NULL;
 	}
-	*head = NULL;
 }
