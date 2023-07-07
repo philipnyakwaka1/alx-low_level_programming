53c909887566ce310ec35721677f575c6fc643fa
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-07-07 11:17:55 +0000

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,31 +1,63 @@
 #include "main.h"
 /**
+ * _strlen - prints string length
+ * @str: input string
+ * Return: string length
+ */
+int _strlen(char *str)
+{
+	int x;
+
+	x = 0;
+	while (str[x] != '\0')
+	{
+		x++;
+	}
+	return (x);
+}
+/**
  * print_binary - prints binary
  * @n: input number
  */
 void print_binary(unsigned long int n)
 {
-	size_t val;
-	int a, b;
+	char *s;
+	int i, j, bin, len;
+	size_t num;
 
-	a = 63;
-	b = 0;
-	while (a >= 0)
-	{
-		val = n >> a;
-		if (1 & val)
-		{
-			_putchar('1');
-			b = b + 1;
-		}
-		if (b)
-		{
-			_putchar('0');
-		}
-		a--;
-	}
-	if (!b)
+	i = 0;
+	num = n;
+	if (n == 0)
 	{
 		_putchar('0');
 	}
+	while (num > 0)
+	{
+		num = num >> 1;
+		i++;
+	}
+	s = malloc(sizeof(char) * (i));
+	i = 0;
+	while (n > 0)
+	{
+		bin = n & 1;
+		if (bin)
+		{
+			s[i] = '1';
+		}
+		else
+		{
+			s[i] = '0';
+		}
+		n = n >> 1;
+		i++;
+	}
+	s[i] = '\0';
+	len = _strlen(s);
+	for (j = len - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	free(s);
+
 }
