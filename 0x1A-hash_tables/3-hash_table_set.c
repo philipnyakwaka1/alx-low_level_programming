86ddea3d15fcab90312bebdb8f93beb51579bb11
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-09-25 01:33:09 +0300

    node end

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,26 +16,37 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
+		return (0);
+	new->key = strdup(key);
+	if (new->key == NULL)
 	{
+		free(new);
 		return (0);
 	}
-	new->key = strdup(key);
 	new->value = strdup(value);
+	if (new->value == NULL)
+	{
+		free(new);
+		return (0);
+	}
 	new->next = NULL;
 	index = key_index((const unsigned char *)key, ht->size);
 	ptr = ht->array[index];
 	if (ptr == NULL)
 		ptr = new;
-	else if (ptr != NULL)
+	while (ptr != NULL)
 	{
 		if (strcmp(ptr->key, key) == 0)
 		{
 			free(ptr->value);
 			ptr->value = strdup(value);
+			if (ptr->value == NULL)
+				return (0);
+		return (1);
 		}
-		while (ptr->next != NULL)
-			ptr = ptr->next;
-		ptr = new;
+
+		ptr = ptr->next;
 	}
+	ptr->next = new;
 	return (1);
 }
