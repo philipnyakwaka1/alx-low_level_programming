677d5406d2793590eb3a46adb17bb3d89b8e591d
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-10-25 22:15:15 +0300

    area

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
 """Module to calculate perimeter"""
 
+
 def island_perimeter(grid):
     """
     Calculate the perimeter of the island described in grid.
     """
     perimeter = 0
+    row = 0
 
-    for row in range(0, len(grid)):
-        for col in range(0, len(grid[0])):
+    while row < len(grid):
+        col = 0
+        while col < len(grid[0]):
             if grid[row][col] == 1:
-                perimeter = perimeter + 4
+                perimeter += 4
                 if row > 0 and grid[row - 1][col] == 1:
-                    perimeter = perimeter - 2
-                elif col > 0 and grid[row][col - 1] == 1:
-                    perimeter = perimeter - 2
+                    perimeter -= 2
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
+            col += 1
+        row += 1
 
     return perimeter
