fae8b20d7d06660d1adbff33589160d9a10cabc5
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-11-05 13:37:04 +0300

    add node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int index_count;
 
 	new = malloc(sizeof(listint_t));
-	if (!new || !head || !*head)
+	if (!new || !head)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
