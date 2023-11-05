5922985267ac47b98e03a72f35bcafb724e51138
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-11-05 09:59:58 +0300

    delete head node

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,11 +9,14 @@ int pop_listint(listint_t **head)
 	listint_t *tmp;
 	int n;
 
-	if (!head)
+	if (*head == NULL)
 		return (0);
-	tmp = (*head)->next;
-	n = (*head)->n;
-	free(*head);
-	*head = tmp;
+	if (head)
+	{
+		tmp = (*head)->next;
+		n = (*head)->n;
+		free(*head);
+		*head = tmp;
+	}
 	return (n);
 }
