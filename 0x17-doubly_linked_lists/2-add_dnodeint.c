6d6d70e9ae4e67b91e32e3b7de21bf9074b9f9e5
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2023-11-05 16:23:59 +0300

    add node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,7 +14,15 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	new->n = n;
 	new->prev = NULL;
-	new->next = *head;
-	*head = new;
+	if (*head)
+	{
+		new->next = *head;
+		*head = new;
+	}
+	else
+	{
+		new->next = NULL;
+		*head = new;
+	}
 	return (new);
 }
