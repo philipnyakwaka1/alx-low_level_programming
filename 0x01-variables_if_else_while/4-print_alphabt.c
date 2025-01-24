2757764d969a2c29fa2c34667b52113dfaea1e11
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2025-01-24 13:39:41 +0300

    putchar

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - the main functions prints a-z
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'e' || c == 'q')
+			continue;
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
