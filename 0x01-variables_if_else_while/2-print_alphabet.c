be2cee6be0b3996552244ec9ed6318bccd3163b2
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2025-01-24 13:29:42 +0300

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - the main functions prints a-z
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
